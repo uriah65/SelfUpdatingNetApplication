27f741ed684e56b1ce9104f64ef77b9322dea4ca
Uriah65 <uriah65@users.noreply.github.com>
2015-08-01 12:18:43 -0400

    .

---
+++ Upgrader/UpgraderUnitTests/ManualTests/DeployToBase.cmd
@@ -0,0 +1,5 @@
+copy ..\..\..\WpfApplication\bin\Debug\WpfApplication.exe C:\temp\UpgraderTests\ApplicationBase\WpfApplication.exe
+copy ..\..\..\WpfApplication\bin\Debug\UpgraderLib.dll C:\temp\UpgraderTests\ApplicationBase\UpgraderLib.dll
+copy ..\..\..\Upgrader\bin\Debug\Upgrader.exe C:\temp\UpgraderTests\ApplicationBase\Upgrader.exe
+copy Upgrader.exe.config C:\temp\UpgraderTests\ApplicationBase\Upgrader.exe.config
+pause
---
+++ Upgrader/UpgraderUnitTests/ManualTests/DeployToExe.cmd
@@ -0,0 +1,5 @@
+copy ..\..\..\WpfApplication\bin\Debug\WpfApplication.exe C:\temp\UpgraderTests\ApplicationExe\WpfApplication.exe
+copy ..\..\..\WpfApplication\bin\Debug\UpgraderLib.dll C:\temp\UpgraderTests\ApplicationExe\UpgraderLib.dll
+copy ..\..\..\Upgrader\bin\Debug\Upgrader.exe C:\temp\UpgraderTests\ApplicationExe\Upgrader.exe
+copy Upgrader.exe.config C:\temp\UpgraderTests\ApplicationExe\Upgrader.exe.config
+pause
